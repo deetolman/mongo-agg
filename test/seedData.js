0667d02d71c7775c35fd0ab276df232fbd1d42f3
Dyanna Tolman <dyannast@gmail.com>
2019-01-31 21:19:23 -0800

    refactored some lines in seedData

--- test/seedData.js
+++ test/seedData.js
@@ -1,19 +1,18 @@
 const Tweet = require('../lib/models/Tweet');
 const User = require('../lib/models/User');
 const Chance = require('chance');
-
 const chance = new Chance();
 
-const TOTAL_USERS = 5;
-const TOTAL_TWEETS = 10;
+const DEFAULT_TOTAL_USERS = 5;
+const DEFAULT_TOTAL_TWEETS = 100;
 
-module.exports = () => {  
+module.exports = () => ({ totalUsers = DEFAULT_TOTAL_USERS, totalTweets = DEFAULT_TOTAL_TWEETS }) => { 
   return Promise.all(
-    [...Array(TOTAL_USERS)].map((ele, i) => User.create({ email: `test${i}@test.com`, password: 'password' }))
+    [...Array(totalUsers)].map((ele, i) => User.create({ email: `test${i}@test.com`, password: 'password' }))
   )
     .then(users => {
       return Promise.all(
-        [...Array(TOTAL_TWEETS)].map(() => {
+        [...Array(totalTweets)].map(() => {
           return Tweet.create({
             handle: chance.pickone(users)._id,
             text: chance.sentence()

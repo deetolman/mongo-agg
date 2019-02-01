790645a09924cefee2e381c6254a78daeb66a7a8
Dyanna Tolman <dyannast@gmail.com>
2019-01-31 21:44:32 -0800

    lint errors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 require('./lib/utils/connect')();
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
-app.listen(PORT, (a) => {
+app.listen(PORT, () => {
   console.log('LISTENING on', PORT);
 });
